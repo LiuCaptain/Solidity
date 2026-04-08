c1a746e9df2fc21988cb5151cfd5ec4c49b400b2
Captain <43247357+LiuCaptain@users.noreply.github.com>
2026-04-08 10:27:50 +0800

    添加 struct 变量

--- contracts/4-10.StructType.sol
+++ contracts/4-10.StructType.sol
@@ -3,5 +3,17 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract StructType {
+    struct Home {
+        string country;
+        string province;
+        string city;
+    }
+
+    struct Person {
+        string name;
+        uint8 age;
+        Home home;
+    }
+
     
 }
