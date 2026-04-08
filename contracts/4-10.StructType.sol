e90f3315488fbb03a8ba8d09b0dd3517f478c1a1
Captain <43247357+LiuCaptain@users.noreply.github.com>
2026-04-08 14:54:16 +0800

    添加 4-10 节课程代码

--- contracts/4-10.StructType.sol
+++ contracts/4-10.StructType.sol
@@ -15,5 +15,33 @@ contract StructType {
         Home home;
     }
 
-    
+    Person master;
+
+    // Struct 类型 - 读取
+    function testReadStruct () public view returns(Person memory) {
+        return master;
+    }
+
+    // Struct 类型 - 写入
+    function testWrite (Person calldata p) public {
+        master = p;
+    }
+    function testWriteName (string calldata name) public {
+        master.name = name;
+    }
+
+    // Struct 类型 - Memory 写入
+    function testWriteMemory () public pure returns (Person memory) {
+        Person memory p;
+        p.name = "DaNiu";
+        p.age = 18;
+        return p;
+    }
+    // Struct 类型 - Storage 写入
+    function testWriteStorage () public  returns (Person memory) {
+        Person storage p = master; // 局部的 Struct 类型的 storage 变量需要初始化
+        p.name = "TieZhu";
+        p.age = 20;
+        return p;
+    }
 }
