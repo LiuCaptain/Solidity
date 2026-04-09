5019e6b781a0564316906446045c06d57aa69141
Captain <43247357+LiuCaptain@users.noreply.github.com>
2026-04-09 17:43:36 +0800

    添加 4-12 课程代码

--- contracts/4-12.BytesAndString.sol
+++ contracts/4-12.BytesAndString.sol
@@ -3,5 +3,30 @@
 pragma solidity >=0.7.0 <0.9.0;
 
 contract BytesAndyString {
-    
+    // 初始化方式 - 字面量
+    string strOne = "strOne";
+    bytes bytOne = "bytOne";
+
+    // 初始化方式 - new 操作符
+    // string str = new string(5);
+    // bytes byt = new bytes(5);
+
+    function testBytesAndString () public returns(bytes memory) {
+        string memory strTwo = "strTwo";
+        bytes memory bytTwo = "bytTwo";
+
+        // 不同 location 的拷贝
+        strOne = strTwo;
+        bytOne = bytTwo;
+
+        // 强制类型转换
+        bytTwo = bytes(strTwo);
+        strTwo = string(bytTwo);
+
+        // 动态字节数组的修改和访问
+        bytTwo[0] = 0x88;
+        // bytes1 b = bytTwo[0];
+        
+        return bytTwo;
+    }
 }
