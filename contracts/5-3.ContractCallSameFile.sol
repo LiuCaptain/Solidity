0daf7092d834ed5ef4f9d7a4c0508503e5022e07
Captain <43247357+LiuCaptain@users.noreply.github.com>
2026-04-18 10:15:53 +0800

    添加 5-3 课程代码

---
+++ contracts/5-3.ContractCallSameFile.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Callee {
+    uint256 a;
+
+    function setA (uint256 b) public {
+        a = b;
+    }
+}
+
+contract Caller {
+    address calleeAddress;
+    
+    constructor (address addr) {
+        calleeAddress = addr;
+    }
+
+    function setCalleeAddress (uint256 c) public {
+        Callee collee = Callee(calleeAddress);
+        collee.setA(c);
+    }
+}
