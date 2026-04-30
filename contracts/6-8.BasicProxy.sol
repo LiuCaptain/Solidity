1b5b44ccc586f723eba41ca985ead33daf4cb78e
Captain <43247357+LiuCaptain@users.noreply.github.com>
2026-04-30 16:41:21 +0800

    添加 6-8 课程代码

---
+++ contracts/6-8.BasicProxy.sol
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract LogicV1 {
+    uint256 public placeholder;
+    uint256 public count;
+
+    function serVar () external {
+        count += 1;
+    }
+}
+
+contract LogicV2 {
+    uint256 public placeholder;
+    uint256 public count;
+
+    function setVar () external {
+        count += 2;
+    }
+
+}
+
+interface Logic {
+    function setVar() external;    
+}
+
+contract BasicProxy {
+    address public logic;
+    uint256 public count;
+
+    constructor (address addr) {
+        logic = addr;
+    }
+
+    fallback () external {
+        (bool status,) =  logic.delegatecall(msg.data);
+        require(status, "Call Failed!");
+    }
+}
