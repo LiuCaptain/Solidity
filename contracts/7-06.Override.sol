99bcab8b5dc311a443260532a0fc91b31a190703
Captain <43247357+LiuCaptain@users.noreply.github.com>
2026-05-25 21:20:08 +0800

    修改 7-06 课程 foo => bar

--- contracts/7-06.Override.sol
+++ contracts/7-06.Override.sol
@@ -21,7 +21,7 @@ contract C is A {
 contract D is B, C {
     function foo () public override(B, C) {} // 继承 B 和 C 合约的 foo 函数需要显式 override
 
-    function bar () public override(B, A) {} // 继承 B 和 A 合约的 foo 函数需要显式 override
+    function bar () public override(B, A) {} // 继承 B 和 A 合约的 bar 函数需要显式 override
 }
 // 多重合约继承中，如果子合约从多个父合约继承到了同一函数的多个实现，则必须显式对该函数进行 override，以消除继承歧义
 // C3 线性化结果：A -> B -> C -> D
